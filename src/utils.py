a450a2353c33dedb79edd5beabed2a1adb9be10f
Kunal Gupta <kunal.gupta@bigohtech.com>
2024-02-17 16:57:18 +0530

    feat:predict pipeline

--- src/utils.py
+++ src/utils.py
@@ -46,3 +46,11 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
             return report
     except Exception as e:
         CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file:
+            return dill.load(file)
+        
+    except Exception as e:
+        raise CustomException(e,sys)
